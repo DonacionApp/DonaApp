c3433a6bed575f391a3afe199cac4f07640191f2
Ilesandres <ilesandres6@gmail.com>
2025-10-18 16:35:56 -0500

    enviar correo para restauracion de contraseña

--- backend/src/config/seder-service/seder-service.service.ts
+++ backend/src/config/seder-service/seder-service.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { template } from 'handlebars';
 import { MailService } from 'src/core/mail/mail.service';
 import { RolEntity } from 'src/modules/rol/entity/rol.entity';
 import { StatusDonationEntity } from 'src/modules/statusdonation/entity/status.donation.entity';
@@ -32,106 +31,103 @@ export class SederServiceService {
         @InjectRepository(TypeReportEntity)
         private readonly typeReportRepository: Repository<TypeReportEntity>,
         private readonly mailService: MailService,
-    ){}
+    ) { }
 
-    async onModuleInit(){
+    async onModuleInit() {
         const countRol = await this.rolRepository.count();
-        if(countRol === 0){
+        if (countRol === 0) {
             await this.rolRepository.save([
-                {rol:'admin'},
-                {rol:'user'},
-                {rol:'organizacion'}
+                { rol: 'admin' },
+                { rol: 'user' },
+                { rol: 'organizacion' }
             ]);
             console.log('Roles iniciales creados');
         };
 
         const countTypeDni = await this.typeDniRepository.count();
-        if(countTypeDni === 0){
+        if (countTypeDni === 0) {
             await this.typeDniRepository.save([
-                {type:'NIT'},
-                {type:'CC'}
+                { type: 'NIT' },
+                { type: 'CC' }
             ]);
             console.log('Type DNI iniciales creados');
         };
 
         const countTags = await this.tagsRepository.count();
-        if(countTags === 0){
+        if (countTags === 0) {
             await this.tagsRepository.save([
-                {tag:'ropa'},
-                {tag:'alimentos'},
-                {tag:'medicamentos'},
-                {tag:'dinero'},
-                {tag:'electronicos'}
+                { tag: 'ropa' },
+                { tag: 'alimentos' },
+                { tag: 'medicamentos' },
+                { tag: 'dinero' },
+                { tag: 'electronicos' }
             ]);
             console.log('Tags iniciales creados');
         };
-        
+
         const countStatusDonation = await this.statusDonationRepository.count();
-        if(countStatusDonation === 0){
+        if (countStatusDonation === 0) {
             await this.statusDonationRepository.save([
-                {status:'pendiente'},
-                {status:'aceptada'},
-                {status:'rechazada'},
-                {status:'entregada'}
+                { status: 'pendiente' },
+                { status: 'aceptada' },
+                { status: 'rechazada' },
+                { status: 'entregada' }
             ]);
             console.log('Status Donation iniciales creados');
         };
 
         const countTypeMessage = await this.typeMessageRepository.count();
-        if(countTypeMessage === 0){
+        if (countTypeMessage === 0) {
             await this.typeMessageRepository.save([
-                {type:'texto'},
-                {type:'imagen'},
-                {type:'video'},
-                {type:'audio'},
-                {type:'documento'}
+                { type: 'texto' },
+                { type: 'imagen' },
+                { type: 'video' },
+                { type: 'audio' },
+                { type: 'documento' }
             ]);
             console.log('Type Message iniciales creados');
         };
 
         const countTypeNotify = await this.typeNotifyRepository.count();
-        if(countTypeNotify === 0){
+        if (countTypeNotify === 0) {
             await this.typeNotifyRepository.save([
-                {type:'informaacion'},
-                {type:'alerta'},
-                {type:'recordatorio'},
+                { type: 'informaacion' },
+                { type: 'alerta' },
+                { type: 'recordatorio' },
             ]);
             console.log('Type Notify iniciales creados')
         };
 
         const countTypePost = await this.typePostRepository.count();
-        if(countTypePost ===0 ){
+        if (countTypePost === 0) {
             await this.typePostRepository.save([
-                {type:'donacion'},
-                {type:'publicacion'},
+                { type: 'donacion' },
+                { type: 'publicacion' },
             ]);
             console.log('typost iniciales creados con exito')
         };
 
         const countTypeReport = await this.typeReportRepository.count();
-        if(countTypeReport === 0){
+        if (countTypeReport === 0) {
             await this.typeReportRepository.save([
-                {type:'solicitud'},
-                {type: 'cumplimiento'},
-                {type: 'entrega'}
+                { type: 'solicitud' },
+                { type: 'cumplimiento' },
+                { type: 'entrega' }
             ])
         }
 
         const testSendEmailResetPassword = {
-    to: 'bailess22@itp.edu.co',
-    subject: 'Restablece tu contraseña',
-    type: 'reset-password',
-    context: {
-        user: 'Andrés Iles',
-        idUser: 101, //no requerido
-        code: '654321',
-        url: 'https://miapp.com/reset-password/101',
-    },
-}
-await this.mailService.sendMail(testSendEmailResetPassword);
-
-console.log('Correo de restablecimiento de contraseña enviado con éxito');
-
+            to: 'bailess22@itp.edu.co',
+            subject: 'Restablece tu contraseña',
+            type: 'reset-password',
+            context: {
+                user: 'Andrés Iles',
+                idUser: 101,
+                code: '654321',
+                url: 'https://miapp.com/reset-password/101',
+            },
+        }
+        await this.mailService.sendMail(testSendEmailResetPassword);
 
     }
 }
