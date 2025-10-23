5e2716dac4cb5c508b273b9c29571696c849303f
Ilesandres <ilesandres6@gmail.com>
2025-10-22 21:58:09 -0500

    enpoints protegidos para admin de rol

--- backend/src/modules/rol/rol.controller.ts
+++ backend/src/modules/rol/rol.controller.ts
@@ -1,11 +1,14 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, UseGuards } from '@nestjs/common';
 import { RolService } from './rol.service';
 import { CreateRolDto } from './dto/create.rol.dto';
 import { UpdateRolDto } from './dto/update.rol.dto';
+import { JwtAuthGuard } from 'src/shared/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/shared/guards/roles.guard';
+import { Roles } from '../auth/decorators/roles.decorators';
 
 @Controller('rol')
 export class RolController {
-  constructor(private readonly rolService: RolService) {}
+  constructor(private readonly rolService: RolService) { }
 
   @Get()
   async findAll() {
@@ -22,16 +25,22 @@ export class RolController {
     return await this.rolService.findByName(rol);
   }
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('admin')
   @Post()
   async create(@Body() dto: CreateRolDto) {
     return await this.rolService.create(dto);
   }
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('admin')
   @Post('update/:id')
   async update(@Param('id') id: number, @Body() dto: UpdateRolDto) {
     return await this.rolService.update(Number(id), dto);
   }
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('admin')
   @Delete('delete/:id')
   async delete(@Param('id') id: number) {
     return await this.rolService.delete(Number(id));
