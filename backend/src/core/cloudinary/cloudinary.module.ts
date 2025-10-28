3c5d370897be6e19139aa6d8235e7be4364f7f02
Ilesandres <ilesandres6@gmail.com>
2025-10-28 08:23:37 -0500

    configuracion cloudianry modulo

--- backend/src/core/cloudinary/cloudinary.module.ts
+++ backend/src/core/cloudinary/cloudinary.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { ClodinaryController } from './cloudinary.controller';
 import { ClodinaryService } from './cloudinary.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { v2 as cloudinary } from 'cloudinary';
+import { CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET, CLOUDINARY_NAME } from 'src/config/constants';
 
 @Module({
+  imports: [ConfigModule],
   controllers: [ClodinaryController],
-  providers: [ClodinaryService]
+  providers: [ClodinaryService,
+    {
+      provide: 'CLOUDINARY',
+      inject:[ConfigService],
+      useFactory:(configService:ConfigService)=>{
+        return cloudinary.config({
+          cloud_name: configService.get<string>(CLOUDINARY_NAME),
+          api_key: configService.get<string>(CLOUDINARY_API_KEY),
+          api_secret: configService.get<string>(CLOUDINARY_API_SECRET),
+        })
+      }
+    }
+  ]
 })
 export class ClodinaryModule {}
