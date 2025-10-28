2618157b31822f5e3ab9d769700a04a320cf284d
Ilesandres <ilesandres6@gmail.com>
2025-10-28 09:42:06 -0500

    validaciones de archivos

--- backend/src/core/cloudinary/cloudinary.controller.ts
+++ backend/src/core/cloudinary/cloudinary.controller.ts
@@ -4,18 +4,4 @@ import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('cloudinary')
 export class CloudinaryController {
-    constructor(
-        private readonly cloudinaryService: CloudinaryService
-    ){}
-
-    @Post('upload/image')
-    @UseInterceptors(FileInterceptor('profile'))
-    async uploadFile(@Body('folder') folder:string, @UploadedFile() file: Express.Multer.File){
-        if(file)console.log('archivo recibido en el controlador de cloudinary:');
-        if(!file || !folder) {
-            console.log('no se recibio archivo o carpeta');
-            return;
-        }
-        return this.cloudinaryService.uploadImage(folder, file);
-    }
 }
