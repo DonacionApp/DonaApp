b850590cbd221e0101d42708d5515d761f3da4a2
Kevin <kevinchanchi02@gmail.com>
2025-10-29 16:13:46 -0500

    Configuracion del modulo de TypeNotify

--- backend/src/modules/typenotify/typenotify.module.ts
+++ backend/src/modules/typenotify/typenotify.module.ts
@@ -1,4 +1,13 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { TypeNotifyEntity } from "./entity/type.notify.entity";
+import { TypeNotifyController } from "./typenotify.controller";
+import { TypeNotifyService } from "./typenotify.service";
 
-@Module({})
+@Module({
+   imports: [TypeOrmModule.forFeature([TypeNotifyEntity])],
+   controllers: [TypeNotifyController],
+   providers: [TypeNotifyService],
+   exports: [TypeNotifyService],
+})
 export class TypeNotifyModule {}
