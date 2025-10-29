b03be199ac9d96539aa23f400124ad41d012ad89
Ilesandres <ilesandres6@gmail.com>
2025-10-29 11:06:38 -0500

    buscar todos los tipos

--- backend/src/modules/typepost/typepost.service.ts
+++ backend/src/modules/typepost/typepost.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { TypePostEntity } from './entity/type.port.entity';
 import { Repository } from 'typeorm';
@@ -9,4 +9,16 @@ export class TypepostService {
         @InjectRepository(TypePostEntity)
         private readonly typePostRespository: Repository<TypePostEntity>,
     ){}
+
+    async findAll():Promise<TypePostEntity[]>{
+        try {
+            const types= await this.typePostRespository.find();
+            if(!types){
+                throw new NotFoundException('no se encontraron los tipos de post')
+            }
+            return types;
+        } catch (error) {
+            throw error;
+        }
+    }
 }
