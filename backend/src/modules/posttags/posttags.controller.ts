ffb13a7ba53ab3bc2570cb73ebb8b0ca70ba64f6
Ilesandres <ilesandres6@gmail.com>
2025-10-29 19:35:58 -0500

    agregar metodos de postatg, y crear endpoints

--- backend/src/modules/posttags/posttags.controller.ts
+++ backend/src/modules/posttags/posttags.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { PosttagsService } from './posttags.service';
 
 @Controller('posttags')
-export class PosttagsController {}
+export class PosttagsController {
+    constructor(
+        private readonly postTagsService: PosttagsService
+    ) { }
+
+    @Get(':tagId/posts')
+    async getPostsByTagId(@Param('tagId') tagId: number): Promise<any[]> {
+        try {
+            return this.postTagsService.getPostsbyTagId(tagId);
+        } catch (error) {
+            throw error;
+        }
+    }
+    @Get('post/:postId/tags')
+    async getTagsByPostId(@Param('postId') postId: number) {
+        try {
+            return this.postTagsService.getTagsByPostId(postId);
+        } catch (error) {
+            throw error;
+        }
+    }
+}
