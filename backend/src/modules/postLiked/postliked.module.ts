7712a85b0d168f82ce8d3d5189c2d15db8d71516
Ilesandres <ilesandres6@gmail.com>
2025-10-31 08:38:51 -0500

    imports y exports base en modulo

--- backend/src/modules/postLiked/postliked.module.ts
+++ backend/src/modules/postLiked/postliked.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
-import { PostlikedController } from './postliked.controller';
-import { PostlikedService } from './postliked.service';
+import { PostlikedController } from '../postLiked/postliked.controller';
+import { PostlikedService } from '../postLiked/postliked.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostLikedEntity } from './entity/post.liked.entity';
+import { PostModule } from '../post/post.module';
 
 @Module({
+  imports:[TypeOrmModule.forFeature([PostLikedEntity]),PostModule],
   controllers: [PostlikedController],
-  providers: [PostlikedService]
+  providers: [PostlikedService],
+  exports:[PostlikedService]
 })
 export class PostlikedModule {}
