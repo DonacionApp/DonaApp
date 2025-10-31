0b7078108f883531dd73c191d85275b6124dbe0a
Ilesandres <ilesandres6@gmail.com>
2025-10-31 08:48:57 -0500

    metodo para agregar like a una publicacion

--- backend/src/modules/postLiked/postliked.module.ts
+++ backend/src/modules/postLiked/postliked.module.ts
@@ -4,9 +4,10 @@ import { PostlikedService } from '../postLiked/postliked.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostLikedEntity } from './entity/post.liked.entity';
 import { PostModule } from '../post/post.module';
+import { UserModule } from '../user/user.module';
 
 @Module({
-  imports:[TypeOrmModule.forFeature([PostLikedEntity]),PostModule],
+  imports:[TypeOrmModule.forFeature([PostLikedEntity]),PostModule,UserModule],
   controllers: [PostlikedController],
   providers: [PostlikedService],
   exports:[PostlikedService]
--- backend/src/modules/postLiked/postliked.service.ts
+++ backend/src/modules/postLiked/postliked.service.ts
@@ -1,4 +1,57 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { PostLikedEntity } from './entity/post.liked.entity';
+import { Repository } from 'typeorm';
+import { PostService } from '../post/post.service';
+import { UserService } from '../user/user.service';
 
 @Injectable()
-export class PostlikedService {}
+export class PostlikedService {
+    constructor(
+        @InjectRepository(PostLikedEntity)
+        private readonly postLikedRepository: Repository<PostLikedEntity>,
+        private readonly postService:PostService,
+        private readonly userService:UserService,
+    ){}
+
+    async addLikeToPost(userId:number, postId:number):Promise<PostLikedEntity>{
+        try {
+            if(!userId || userId<=0 || userId===undefined || userId===null || isNaN(userId)){
+                throw new BadRequestException('no se ha recibido correctamente el usuario');
+            }
+            if(!postId || postId<=0 || postId===undefined || postId===null || isNaN(postId)){
+                throw new BadRequestException('no se ha recibido correctamente el post');
+            }
+            postId=Number(postId);
+            userId=Number(userId);
+            const post =  await this.postService.getPostById(postId);
+            if(!post){
+                throw new BadRequestException('el post al que se le quiere dar like no existe');
+            }
+            const user= await this.userService.findById(userId);
+            if(!user){
+                throw new BadRequestException('el usuario que quiere dar like no existe');
+            }
+            const postLikedExist= await this.postLikedRepository.findOne({
+                where:{
+                    post:{
+                        id:postId
+                    },
+                    user:{
+                        id:userId
+                    }
+                }
+            });
+            if(postLikedExist){
+                throw new BadRequestException('el usuario ya le ha dado like a este post');
+            };
+            const postliked=this.postLikedRepository.create({
+                post:post,
+                user:user
+            });
+            return await this.postLikedRepository.save(postliked);
+        } catch (error) {
+            throw error;
+        }
+    }
+}
