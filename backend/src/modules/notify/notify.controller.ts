1fc62e21ed76302525e35745f0b9b4119d09e509
Kevin <kevinchanchi02@gmail.com>
2025-10-31 20:41:29 -0500

    Agregar endpoint al NotifyController para la creacion de notificaciones

--- backend/src/modules/notify/notify.controller.ts
+++ backend/src/modules/notify/notify.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Post, UseGuards, UsePipes, ValidationPipe } from "@nestjs/common";
+import { NotifyService } from "./notify.service";
+import { JwtAuthGuard } from "src/shared/guards/jwt-auth.guard";
+import { RolesGuard } from "src/shared/guards/roles.guard";
+import { Roles } from "../auth/decorators/roles.decorators";
+import { CreateNotifyDto } from "./dto/create.notify.dto";
 
+@UsePipes(new ValidationPipe())
 @Controller('notify')
-export class NotifyController {}
+export class NotifyController {
+   constructor(
+      private readonly notifyService: NotifyService,
+   ) { }
+
+   @UseGuards(JwtAuthGuard, RolesGuard)
+   @Roles('admin')
+   @Post('create')
+   async createNotify(@Body() dto: CreateNotifyDto) {
+      return await this.notifyService.createNotify(dto);
+   }
+}
