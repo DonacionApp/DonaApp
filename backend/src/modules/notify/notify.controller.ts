ae5891ab7fac74496691607ffc8a94df079e4c79
Kevin <kevinchanchi02@gmail.com>
2025-11-01 10:55:50 -0500

    crear endpoint en el NotifyController para buscar notificacion por id

--- backend/src/modules/notify/notify.controller.ts
+++ backend/src/modules/notify/notify.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UseGuards, UsePipes, ValidationPipe } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post, UseGuards, UsePipes, ValidationPipe } from "@nestjs/common";
 import { NotifyService } from "./notify.service";
 import { JwtAuthGuard } from "src/shared/guards/jwt-auth.guard";
 import { RolesGuard } from "src/shared/guards/roles.guard";
@@ -17,5 +17,12 @@ export class NotifyController {
    @Post('create')
    async createNotify(@Body() dto: CreateNotifyDto) {
       return await this.notifyService.createNotify(dto);
+   }
+
+   @UseGuards(JwtAuthGuard, RolesGuard)
+   @Roles('admin')
+   @Get('id/:id')
+   async getNotifyById(@Param('id') id: number) {
+      return await this.notifyService.findById(id);
    }
 }
