f87d9e59c6298f62d75026acf077c59342934de1
Kevin <kevinchanchi02@gmail.com>
2025-10-31 20:52:53 -0500

    Configurar NotifyModule

--- backend/src/modules/notify/notify.module.ts
+++ backend/src/modules/notify/notify.module.ts
@@ -1,4 +1,17 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { NotifyEntity } from "./entity/notify.entity";
+import { TypeNotifyModule } from "../typenotify/typenotify.module";
+import { NotifyService } from "./notify.service";
+import { NotifyController } from "./notify.controller";
 
-@Module({})
+@Module({
+   imports: [
+      TypeOrmModule.forFeature([NotifyEntity]),
+      TypeNotifyModule,
+   ],
+   providers: [NotifyService],
+   controllers: [NotifyController],
+   exports: [NotifyService],
+})
 export class NotifyModule {}
