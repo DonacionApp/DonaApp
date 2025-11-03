cc2e3f6686e2b20d856912250ee7b58e65c6963f
Ilesandres <ilesandres6@gmail.com>
2025-11-03 16:16:46 -0500

    metodos de articulo

--- backend/src/modules/article/article.controller.ts
+++ backend/src/modules/article/article.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ArticleService } from './article.service';
+import { ArticleEntity } from './entity/article.entity';
+import { FiltersDtoArticles } from './dto/filters.dto.articles';
 
 @Controller('article')
-export class ArticleController {}
+export class ArticleController {
+    constructor(
+        private readonly articleService:ArticleService
+    ){}
+
+    @Post('/all')
+    async getAllArticles(@Body()dto:FiltersDtoArticles):Promise<ArticleEntity[]>{
+        return await this.articleService.getAllArticles(dto)
+    }
+
+}
--- backend/src/modules/article/article.service.ts
+++ backend/src/modules/article/article.service.ts
@@ -1,8 +1,9 @@
-import { Inject, Injectable } from '@nestjs/common';
-import { Repository } from 'typeorm';
+import { BadRequestException, ForbiddenException, Inject, Injectable, NotFoundException } from '@nestjs/common';
+import { Not, Repository } from 'typeorm';
 import { ArticleEntity } from './entity/article.entity';
 import { FiltersDtoArticles } from './dto/filters.dto.articles';
 import { InjectRepository } from '@nestjs/typeorm';
+import { CreateArticleDto } from './dto/create.article.dto';
 
 @Injectable()
 export class ArticleService {
@@ -24,6 +25,112 @@ export class ArticleService {
             }
 
             return await query.getMany();
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async getArticleById(id:number):Promise<ArticleEntity>{
+        try {
+            if(!id || id <= 0 || isNaN(id)|| id===undefined){
+                throw new BadRequestException('articulo invalido');
+            }
+            const article = await this.articleRepository.findOne({
+                where:{
+                    id:id
+                }
+            });
+            if(!article){
+                throw new NotFoundException('articulo no encontrado');
+            }
+            return article;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async getArticleByName(name:string):Promise<ArticleEntity>{
+        try {
+            if(!name || name.trim() === '' || name===undefined){
+                throw new BadRequestException('nombre de articulo invalido')
+            }
+            const article= await this.articleRepository.findOne({
+                where:{
+                    name:name
+                }
+            })
+            if(!article){
+                throw new NotFoundException('articulo no encontrado');
+            }
+            return article;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async createArticle(dto:CreateArticleDto):Promise<ArticleEntity>{
+        try {
+            if(!dto || !dto.name || dto.name.trim() === '' || dto.name===undefined){
+                throw new BadRequestException('datos invalidos para crear articulo');
+            }
+            dto.name= dto.name.trim().toLowerCase();
+            const existArticle= await this.articleRepository.findOne({
+                where:{
+                    name:dto.name
+                }
+            });
+            if(existArticle){
+                throw new BadRequestException('el articulo ya existe');
+            };
+            const newArticle = this.articleRepository.create({
+                name:dto.name
+            })
+            return await this.articleRepository.save(newArticle);
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async updateArticle(id:number, dto:CreateArticleDto):Promise<ArticleEntity>{
+        try {
+            if(!id || id <= 0 || isNaN(id)|| id===undefined){
+                throw new BadRequestException('articulo invalido');
+            }
+            if(!dto || !dto.name || dto.name.trim() === '' || dto.name===undefined){
+                throw new BadRequestException('datos invalidos para actualizar articulo');
+            }
+            dto.name= dto.name.trim().toLowerCase();
+            const article= await this.getArticleById(id);
+            if(!article){
+                throw new NotFoundException('articulo no encontrado');
+            }
+            const existArticle= await this.articleRepository.findOne({
+                where:{
+                    name:dto.name,
+                    id:Not(id)
+                }
+            });
+            if(existArticle){
+                throw new ForbiddenException('el nombre del articulo ya existe');
+            }
+            article.name= dto.name;
+            return await this.articleRepository.save(article);
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async deleteArticle(id:number):Promise<{message:string, status:number}>{
+        try {
+            if(!id || id<=0 || isNaN(id) || id===undefined){
+                throw new BadRequestException('articulo invalido');
+            }
+            const article= await this.getArticleById(id);
+            if(!article){
+                throw new NotFoundException('articulo no encontrado');
+            }
+            await this.articleRepository.delete(id);
+            return {message:'articulo eliminado correctamente', status:200};
         } catch (error) {
             throw error;
         }
