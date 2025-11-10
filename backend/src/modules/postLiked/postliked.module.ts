cb38e8a9406b7b36c641448d3d697a48038b0f5c
Ilesandres <ilesandres6@gmail.com>
2025-11-09 23:07:07 -0500

    emitir notificacion al dar like

--- backend/src/modules/postLiked/postliked.module.ts
+++ backend/src/modules/postLiked/postliked.module.ts
@@ -5,10 +5,17 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostLikedEntity } from './entity/post.liked.entity';
 import { PostModule } from '../post/post.module';
 import { UserModule } from '../user/user.module';
+import { NotifyModule } from '../notify/notify.module';
+import { TypeNotifyModule } from '../typenotify/typenotify.module';
 
 @Module({
-  imports:[TypeOrmModule.forFeature([PostLikedEntity]),
-  forwardRef(()=>PostModule),UserModule],
+  imports:[
+    TypeOrmModule.forFeature([PostLikedEntity]),
+    forwardRef(()=>PostModule),
+    UserModule,
+    NotifyModule,
+    TypeNotifyModule
+  ],
   controllers: [PostlikedController],
   providers: [PostlikedService],
   exports:[PostlikedService]
