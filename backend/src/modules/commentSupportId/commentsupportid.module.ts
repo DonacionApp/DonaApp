11eac0816c2623b5e3405ef1b8cb34cdc62f0126
AudinoPantoja <aapantojar22@itp.edu.co>
2025-11-13 15:10:03 -0500

    implementacion endpoints en commentSupportID

--- backend/src/modules/commentSupportId/commentsupportid.module.ts
+++ backend/src/modules/commentSupportId/commentsupportid.module.ts
@@ -1,13 +1,21 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { CommentsupportidController } from '../commentSupportId/commentsupportid.controller';
 import { CommentsupportidService } from './commentsupportid.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CommentSupportIdEntity } from './entity/comment.supportid.entity';
 import { UserModule } from '../user/user.module';
 import { StatussupportidModule } from '../statussupportid/statussupportid.module';
+import { NotifyModule } from '../notify/notify.module';
+import { TypeNotifyModule } from '../typenotify/typenotify.module';
 
 @Module({
-  imports:[TypeOrmModule.forFeature([CommentSupportIdEntity]), UserModule, StatussupportidModule],
+  imports:[
+    TypeOrmModule.forFeature([CommentSupportIdEntity]),
+    UserModule,
+    StatussupportidModule,
+    forwardRef(() => NotifyModule),
+    forwardRef(() => TypeNotifyModule),
+  ],
   controllers: [CommentsupportidController],
   providers: [CommentsupportidService],
   exports:[CommentsupportidService],
