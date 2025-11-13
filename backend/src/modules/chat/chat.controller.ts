d15e519790586ffb13ebdc88f4a9f8a09283fa42
Ilesandres <ilesandres6@gmail.com>
2025-11-13 14:44:51 -0500

    agregar metodo de cargar todos los chats para admin

--- backend/src/modules/chat/chat.controller.ts
+++ backend/src/modules/chat/chat.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Get, Param, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, Param, Req, UseGuards, Query, Post, Body, BadRequestException } from '@nestjs/common';
 import { ChatService } from './chat.service';
 import { JwtAuthGuard } from 'src/shared/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/shared/guards/roles.guard';
+import { Roles } from '../auth/decorators/roles.decorators';
 
 @Controller('chat')
 export class ChatController {
@@ -13,5 +15,38 @@ export class ChatController {
     async getChatFromDonation(@Param('donationId') donationId: string, @Req() req?: any) {
         const currentUser = req?.user?.id;
         return await this.chatService.getChatFronDonationId(Number(donationId), currentUser);
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Get('/all/me')
+    async getChatsByUserId(@Req() req?: any, @Query() query?: any) {
+        const userId = req?.user?.id;
+        const options = {
+            searchParam: query?.searchParam,
+            cursor: query?.cursor,
+            limit: query?.limit,
+            page: query?.page,
+            offset: query?.offset,
+        };
+        return await this.chatService.getChatsByUserId(Number(userId), options as any);
+    }
+
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Roles('admin')
+    @Post('/admin/list')
+    async getAllChatsAdmin(@Req() req?: any, @Body() body?: any, @Query() query?: any) {
+        const user = req?.user;
+
+        const options = {
+            limit: query?.limit,
+            page: query?.page,
+            offset: query?.offset,
+            cursor: query?.cursor,
+            orderBy: query?.orderBy,
+            order: query?.order,
+        };
+
+        const result = await this.chatService.getAllChatsAdmin(body, options as any);
+        return result;
     }
 }
